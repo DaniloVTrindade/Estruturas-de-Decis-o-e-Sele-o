efb406e9aaf01c4e25fdccc1a3d85fc44ba11e0f
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-09-04 14:37:42 -0300

    adiciona src/questao7.php

---
+++ src/questao7.php
@@ -0,0 +1,17 @@
+<?php
+$valorCompra = floatval(readline("Digite o valor da compra: "));
+
+if ($valorCompra >= 500) {
+    $desconto = 20;
+} elseif ($valorCompra >= 100) {
+    $desconto = 10;
+} else {
+    $desconto = 0;
+}
+
+$valorDesconto = $valorCompra * ($desconto / 100);
+$valorFinal = $valorCompra - $valorDesconto;
+
+echo "Desconto: $desconto%\n";
+echo "Valor final: R$ " . number_format($valorFinal, 2, ",", ".") . "\n";
+?>
