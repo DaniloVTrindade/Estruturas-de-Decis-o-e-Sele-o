00764934c5053f1938c3a8820218e2491c50be80
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-09-04 14:37:32 -0300

    adiciona src/questao2.php

---
+++ src/questao2.php
@@ -0,0 +1,13 @@
+<?php
+$idade = intval(readline("Digite sua idade: "));
+
+if ($idade < 12) {
+    echo "Classificação: criança\n";
+} elseif ($idade < 18) {
+    echo "Classificação: adolescente\n";
+} elseif ($idade < 60) {
+    echo "Classificação: adulto\n";
+} else {
+    echo "Classificação: idoso\n";
+}
+?>
