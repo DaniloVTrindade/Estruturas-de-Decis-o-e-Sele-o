a3bdd9a869e2bad4367bd58bd2230c6f3bae54a1
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-09-04 14:37:40 -0300

    adiciona src/questao6.php

---
+++ src/questao6.php
@@ -0,0 +1,32 @@
+<?php
+$numero1 = floatval(readline("Digite o primeiro número: "));
+$numero2 = floatval(readline("Digite o segundo número: "));
+
+echo "1 - Soma\n";
+echo "2 - Subtração\n";
+echo "3 - Multiplicação\n";
+echo "4 - Divisão\n";
+
+$opcao = intval(readline("Escolha uma operação: "));
+
+switch ($opcao) {
+    case 1:
+        echo "Resultado: " . ($numero1 + $numero2) . "\n";
+        break;
+    case 2:
+        echo "Resultado: " . ($numero1 - $numero2) . "\n";
+        break;
+    case 3:
+        echo "Resultado: " . ($numero1 * $numero2) . "\n";
+        break;
+    case 4:
+        if ($numero2 == 0) {
+            echo "Não é possível dividir por zero\n";
+        } else {
+            echo "Resultado: " . ($numero1 / $numero2) . "\n";
+        }
+        break;
+    default:
+        echo "Opção inválida\n";
+}
+?>
