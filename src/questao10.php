cded2997bf722e44309bef4154e451faaf4cb059
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-09-04 14:37:48 -0300

    adiciona src/questao10.php

---
+++ src/questao10.php
@@ -0,0 +1,25 @@
+<?php
+echo "1 - Pendente\n";
+echo "2 - Em processamento\n";
+echo "3 - Enviado\n";
+echo "4 - Entregue\n";
+
+$status = intval(readline("Digite a opção do status: "));
+
+switch ($status) {
+    case 1:
+        echo "Status: Pendente\n";
+        break;
+    case 2:
+        echo "Status: Em processamento\n";
+        break;
+    case 3:
+        echo "Status: Enviado\n";
+        break;
+    case 4:
+        echo "Status: Entregue\n";
+        break;
+    default:
+        echo "Status inválido\n";
+}
+?>
