cda051e6ecbe729303b85429e9b60d4ff690a959
Danilo Trindade Valente <valentedesousadanilo@gmail.com>
2026-09-04 14:37:46 -0300

    adiciona src/questao9.php

---
+++ src/questao9.php
@@ -0,0 +1,11 @@
+<?php
+$temperatura = floatval(readline("Digite a temperatura: "));
+
+if ($temperatura < 15) {
+    echo "Temperatura fria\n";
+} elseif ($temperatura <= 25) {
+    echo "Temperatura agradável\n";
+} else {
+    echo "Temperatura quente\n";
+}
+?>
